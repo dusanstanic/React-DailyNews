e036002505f85d5e98feca6a29497b03c9943074
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-02-17 13:39:57 +0100

    Add hoc components

---
+++ src/hoc/Layout/Layout.tsx
@@ -0,0 +1,36 @@
+import React, { Component, FunctionComponent } from "react";
+import classes from "./Layout.module.scss";
+
+import Aux from "../Auxiliary/Auxiliary";
+
+import Navigation from "../../components/Navigation/Navigation";
+import Footer from "../../components/Footer/Footer";
+
+interface PropsI {
+  children: any;
+}
+
+class Layout extends Component<{}, FunctionComponent> {
+  render() {
+    return (
+      <Aux>
+        <div className={classes["background"]}></div>
+        <div className={classes["layout__container"]}>
+          <div className={classes["layout__navigation"]}>
+            <Navigation />
+          </div>
+
+          <main className={classes["layout__content"]}>
+            {this.props.children}
+          </main>
+
+          <div className={classes["layout__footer"]}>
+            <Footer />
+          </div>
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
